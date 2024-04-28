c991b57b24e7fe6ea0fe651294882b2f1282adf8
Ajay Kushwaha <ak383834@gmail.com>
2024-04-28 10:04:56 +0530

    folder structured

---
+++ TodoApp/reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section className='bg-slate-900 min-w-screen min-h-screen	'>
+      <h2 className='text-white'>ReduxToolkit-Todo</h2>
+    </section>
+  )
+}
+
+export default App
